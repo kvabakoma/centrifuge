c05e6e0ac684be3b4d62f892c5bffdac212f4ba4
ymitev <ymitev@abaltatech.com>
2018-01-27 00:40:16 +0200

    initial commit

---
+++ Centrifuge/Assets/Switch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+
+    public Animator anim;
+    bool isAccesable;
+    bool isOn;
+    // Use this for initialization
+    void Start()
+    {
+        isAccesable = true;
+        isOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isAccesable)
+        {
+            Debug.Log("press");
+            anim.SetTrigger("Press");
+            anim.SetBool("IsOn", isOn);
+            isOn = !isOn;
+        }
+    }
+}
